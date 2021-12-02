bf418426834d49efe01e45db9a2abbd24bf3b05e
chenq <chenqi@sunwayworld.com>
2021-12-02 11:34:46 +0800

    動畫

--- assets/Script/hero-player.ts
+++ assets/Script/hero-player.ts
@@ -16,24 +16,52 @@ export default class NewClass extends cc.Component {
     // LIFE-CYCLE CALLBACKS:
     private _input = {};
     private _speed:number = 200;
+    private _animation :string = 'player_idle';
 
     onLoad() {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        this.node.getComponent(cc.Animation).on(cc.Animation.EventType.FINISHED,
+            this.animationFinished, this);
+        
     }
 
     onDestroy() {
         cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        this.node.getComponent(cc.Animation).off(cc.Animation.EventType.FINISHED, this.animationFinished, this);
+    }
+
+    animationFinished(event: string, data: any) {
+        console.log(event , data);
     }
 
     onKeyDown(e: any) {
         this._input[e.keyCode] = 1;
+        let ani = 'player_idle';
+        if (this._input[cc.macro.KEY.a] || this._input[cc.macro.KEY.left]) {
+            ani = 'player_a';
+        }else if(this._input[cc.macro.KEY.d] || this._input[cc.macro.KEY.right]){
+            ani = 'player_d';
+        }else if(this._input[cc.macro.KEY.s] || this._input[cc.macro.KEY.down]){
+            ani = 'player_s';
+        }else if(this._input[cc.macro.KEY.w] || this._input[cc.macro.KEY.up]){
+            ani = 'player_w';
+        }
+        this.playAnimation(ani);
+    }
+
+    playAnimation(ani:string){
+        if(this._animation !== ani){
+            this._animation = ani;
+            this.node.getComponent(cc.Animation).play(ani);
+        }
     }
 
     onKeyUp(e: any) {
         this._input[e.keyCode] = 0;
     }
+
 
     start() {
 
